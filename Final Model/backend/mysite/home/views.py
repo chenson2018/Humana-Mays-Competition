f1e848151704d034bd937e3256659cc312249c0d
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2019-11-09 15:16:38 -0600

    Added Annotation

--- Final Model/backend/mysite/home/views.py
+++ Final Model/backend/mysite/home/views.py
@@ -14,19 +14,22 @@ def index(request):
     return HttpResponse(home)
 	
 def patient1(request):
-	html = "Patient 1 <br>\
+	html = "Patient 1 <br><br>\
+	This patient shows significant inclinnation towards long-term opioid therapy at all prescription levels. We suggest that you consider alternative treatments or closely monitor this patient.<br>\
 	<img src=\"http://178.128.68.14/p1.png\" alt=\"logo\">"
 
 	return HttpResponse(html)
 	
 def patient2(request):
-	html = "Patient 2 <br>\
+	html = "Patient 2 <br><br>\
+	This patient shows low inclination towards long-term opioid therapy at the current level you have specified. We believe that you may safely increase the initial supply count.<br>\
 	<img src=\"http://178.128.68.14/p2.png\" alt=\"logo\">"
 
 	return HttpResponse(html)
 	
 def patient3(request):
-	html = "Patient 3 <br>\
+	html = "Patient 3 <br><br>\
+	This patient shows borderline inclination towards long-term opioid therapy at the current level you have specified. We believe that you should decrease the initial supply count.<br>\
 	<img src=\"http://178.128.68.14/p3.png\" alt=\"logo\">"
 
 	return HttpResponse(html)
